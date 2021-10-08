bff13fe39184b7db6913a1e7679c18121a3481d6
Cai <1322423005@qq.com>
2021-10-08 17:23:37 +0800

    Test1

---
+++ src/main/java/top/sehnsucht/ShendunApplication.java
@@ -0,0 +1,13 @@
+package top.sehnsucht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShendunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShendunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/sehnsucht/ShendunApplicationTests.java
@@ -0,0 +1,13 @@
+package top.sehnsucht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShendunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
